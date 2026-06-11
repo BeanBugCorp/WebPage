fb1bcf19f043e244889a8dfa17bbb71da2853a5b
J35HN <jeshua.navaavila@gmail.com>
2026-06-10 18:04:54 -0600

    english and spanish json files with text, working with the .tsx file

--- src/components/ProjectsAndPeople.tsx
+++ src/components/ProjectsAndPeople.tsx
@@ -13,9 +13,9 @@ export default function ProjectsAndPeople() {
                 ref = {headerRef}
                 className = {`${styles.header} ${headerVisible ? styles.visible: ''}`}
             >
-                <span className = {styles.label}>hola</span>
-                <h2 className = {styles.title}>Este es el título</h2>
-                <p className = {styles.desc}>Aqui va todo el texto para describir la cosa</p>
+                <span className = {styles.label}> {t('ProjectsAndPeople.label')} </span>
+                <h2 className = {styles.title}> {t('ProjectsAndPeople.title')} </h2>
+                <p className = {styles.desc}> {t('ProjectsAndPeople.description')} </p>
             </div>
         </section>
     );
